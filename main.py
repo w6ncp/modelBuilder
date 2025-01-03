6be8a0374c6f32c74241cfb2e89b33241ccae436
w6ncp <w6ncpham@gmail.com>
2025-01-02 18:14:27 -0800

    In progress of making name generator fully work

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import json
 from model import Model
 
 def main():
-    testing = Model()
+    testing = Model(seed=1234,number=0)
+    print(testing)
+    testing.update_name("Test Character")
+    print(testing)
+    testing.update_name()
+    testing.import_model_type("source/scdwic_template.json")
     print(testing)
 
 main()
--- model.py
+++ model.py
@@ -1,17 +1,23 @@
 import json, csv, os
+import random
 
 class Model:
     def __init__(self, name:str = "", race = None, gender = None, class_type = None, portrait = None,
                 number:int = 0, seed:int = 0):
-        self.name = name
-        self.race = race
-        self.gender = gender
-        self.class_type = class_type
-        self.portait = portrait
-        self.number = number
-        self.seed = int(str(number) + str(seed))
+        self.__name = None
+        self.__race = race
+        self.__gender = gender
+        self.__class_type = class_type
+        self.__portait = portrait
+        self.__number = number
+        self.__seed = int(str(number) + str(seed))
         self.__model = {}
-    
+        self.__experience = 0
+
+        random.seed(self.__seed)
+        self.__name = self.update_name(name, True)
+
+
     def __repr__(self):
         out = {}
         for attr, value in self.__dict__.items():
@@ -33,7 +39,33 @@ class Model:
     def __request_file(self, prompt:str = "File requested:"):
         print(prompt)
         cus_input = input(">>")
+        cwd = os.getcwd()
         if os.path.exists(cus_input):
             return cus_input
-        else:
-            return None
+        return None
+    
+    def __generate_name(self, name):
+        if name is not None:
+            return name
+        first_names = []
+        last_names = []
+        with open("sources/names.csv") as names_file:
+            names_lists = list(csv.reader(names_file,))
+            first_names = names_lists[0][1:]
+            last_names = names_lists[1][1:]
+        first = str(random.choice(first_names))
+        last = str(random.choice(last_names))
+        return f"{first.title()} {last.title()}"
+    
+    def update_name(self, name:str = None, confirm:bool = False):
+        if not confirm:
+            choice = input("Confirm name change? Y/N: ")
+            if choice.upper() == "Y":
+                confirm = True
+        
+        if name is None and confirm:
+                self.__name = self.__generate_name(None)
+        elif confirm:
+            self.__name = name
+        print(f"Chacter name is {self.__name}.")
+            
