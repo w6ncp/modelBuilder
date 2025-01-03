605c91ad23f5fb379bbc7d93ee107c92237e1740
w6ncp <w6ncpham@gmail.com>
2025-01-02 17:26:32 -0800

    Added 'sources' files, method to load JSON template into character

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-import json
+import json, csv, os
 
 class Model:
-    def __init__(self, name:str = None, race = None, gender = None, class_type = None, portrait = None,
-                number:int = None, seed:int = None):
+    def __init__(self, name:str = "", race = None, gender = None, class_type = None, portrait = None,
+                number:int = 0, seed:int = 0):
         self.name = name
         self.race = race
         self.gender = gender
@@ -18,3 +18,22 @@ class Model:
             out[attr] = value
         return json.dumps(out)
 
+    def import_model_type(self, model_file:str = None):
+        while model_file is None:
+            model_file = self.__request_file("Input path to useable JSON model template:")
+            if model_file is None:
+                print("No file path given.\n")
+                continue
+            if not model_file.lower().endswith(".json"):
+                print(f"Invalid file type {model_file.split(".")[-1]} given.\n")
+                continue
+        self.__model["Template"] = model_file
+
+
+    def __request_file(self, prompt:str = "File requested:"):
+        print(prompt)
+        cus_input = input(">>")
+        if os.path.exists(cus_input):
+            return cus_input
+        else:
+            return None
